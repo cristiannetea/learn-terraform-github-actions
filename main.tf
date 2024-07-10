c236b6ba10e0b719f0c4058ffc68bfb95cda2995
Cristian Netea <cristian_netea@yahoo.com>
2024-07-10 10:37:46 +0300

    test

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
   required_version = "~> 1.0"
 
   backend "remote" {
-    organization = "ACG-Terraform-Demos78"
+    organization = "myipcrnet"
 
     workspaces {
       name = "demo-github-actions"
